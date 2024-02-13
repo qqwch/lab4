d334ac675afabd7c6156277d460d3f6400187304
qqwch <157726082+qqwch@users.noreply.github.com>
2024-02-13 21:19:24 +0200

    Додано функцію is_power_of_five до utils.py та виклик цієї функції у файлі main.py

--- lab4/main.py
+++ lab4/main.py
@@ -1,2 +1,11 @@
-print(f"{number} є простим числом.")
-print(f"{number} не є простим числом.")
+if is_prime(number):
+    print(f"{number} є простим числом.")
+else:
+    print(f"{number} не є простим числом.")
+
+if is_power_of_five(number):
+    print(f"{number} є степенем п'ятірки.")
+else:
+    print(f"{number} не є степенем п'ятірки.")
+
+
---
+++ lab4/utils.py
@@ -0,0 +1,24 @@
+def factorial(n):
+    """Обчислює факторіал числа n."""
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def is_prime(n):
+    """Перевіряє, чи є число n простим."""
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def is_power_of_five(n):
+    """Перевіряє, чи є число n степенем п'ятірки."""
+    if n <= 0:
+        return False
+    while n % 5 == 0:
+        n /= 5
+    return n == 1
